8f3976a771eecebd9c9789a443583e3c0e04b5dd
Jimi2708 <143849768+Jimi2708@users.noreply.github.com>
2023-12-27 08:34:41 -0600

    Update backend.tf

--- JenkinsServer/backend.tf
+++ JenkinsServer/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "jimitechbucket4"
+    bucket = "jimitechbucket5"
     key    = "terraform/backend/jenkins-server/terraform.tfstate"
     region = "us-east-2"
   }
