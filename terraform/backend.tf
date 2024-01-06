81d90d32dfe50086d7eb58f413d661b7a5fac2d2
Jimi2708 <143849768+Jimi2708@users.noreply.github.com>
2024-01-06 15:58:21 -0600

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "jimitechbucketone"
+    bucket = "jimitechbucket6"
     key    = "terraform/backend/eks/terraform.tfstate"
     region = "us-east-1"
   }
